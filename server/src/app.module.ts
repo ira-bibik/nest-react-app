a4f124aef25326253585c50aa41f1ee16f70e569
Iryna Bibik <bibik.iryna004@gmail.com>
2023-10-10 14:57:08 +0300

    created entities for user, category, transaction

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,10 +5,32 @@ import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { AuthModule } from './auth/auth.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, CategoryModule, AuthModule, TransactionModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configSevice: ConfigService) => ({
+                type: 'postgres',
+                host: configSevice.get('DB_HOST'),
+                port: configSevice.get('DB_PORT'),
+                username: configSevice.get('DB_USERNAME'),
+                password: configSevice.get('DB_PASSWORD'),
+                database: configSevice.get('DB_NAME'),
+                synchronize: true,
+                entities: [__dirname + '/**/*.entity{.js, .ts}'],
+            }),
+            inject: [ConfigService],
+        }),
+        UserModule,
+        CategoryModule,
+        AuthModule,
+        TransactionModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
